8c71302ef611b3a2bdf65e703bc99cdc62515b34
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-01-31 23:55:07 +1100

    packer bundle unbundler

--- src/main/java/com/ricedotwho/rsm/mixins/MixinClientPacketListener.java
+++ src/main/java/com/ricedotwho/rsm/mixins/MixinClientPacketListener.java
@@ -1,7 +1,12 @@
 package com.ricedotwho.rsm.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.ricedotwho.rsm.event.impl.client.PacketEvent;
 import com.ricedotwho.rsm.event.impl.player.PlayerChatEvent;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.PacketListener;
+import net.minecraft.network.protocol.Packet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,5 +27,17 @@ public class MixinClientPacketListener {
         if (new PlayerChatEvent(message).post()) {
             ci.cancel();
         }
+    }
+
+    @WrapOperation(
+            method = "handleBundlePacket",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/network/protocol/Packet;handle(Lnet/minecraft/network/PacketListener;)V"
+            )
+    )
+    private void wrapPacketHandle(Packet<?> packet, PacketListener listener, Operation<Void> original) {
+        if (new PacketEvent.Receive(packet).post()) return;
+        original.call(packet, listener);
     }
 }
