13a7f97b987a0cd2c039e01997ceae74cb45573a
Hypericats <email@gmail.com>
2026-02-03 08:06:19 -0500

    Added roomRotation helper function

--- src/main/java/com/ricedotwho/rsm/component/impl/map/map/RoomRotation.java
+++ src/main/java/com/ricedotwho/rsm/component/impl/map/map/RoomRotation.java
@@ -1,9 +1,29 @@
 package com.ricedotwho.rsm.component.impl.map.map;
 
+import net.minecraft.core.Direction;
+
 public enum RoomRotation {
     TOPLEFT,
     TOPRIGHT,
     BOTLEFT,
     BOTRIGHT,
-    UNKNOWN
+    UNKNOWN;
+
+    public Direction toDir() {
+        switch (this) {
+            case TOPLEFT -> {
+                return Direction.NORTH;
+            }
+            case TOPRIGHT -> {
+                return Direction.SOUTH;
+            }
+            case BOTLEFT -> {
+                return Direction.WEST;
+            }
+            case BOTRIGHT -> {
+                return Direction.EAST;
+            }
+        }
+        return null;
+    }
 }
