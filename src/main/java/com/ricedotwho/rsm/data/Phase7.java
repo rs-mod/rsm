faedc7057c8b587c4286e616e4f60b418121843b
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-02-06 19:14:26 +1100

    dungeon phases and p3 sections

---
+++ src/main/java/com/ricedotwho/rsm/data/Phase7.java
@@ -0,0 +1,14 @@
+package com.ricedotwho.rsm.data;
+
+public enum Phase7 {
+    P1,
+    P2,
+    P3,
+    P4,
+    P5,
+    S1,
+    S2,
+    S3,
+    S4,
+    UNKNOWN
+}
---
+++ src/main/java/com/ricedotwho/rsm/utils/DungeonUtils.java
@@ -0,0 +1,73 @@
+package com.ricedotwho.rsm.utils;
+
+import com.ricedotwho.rsm.data.Phase7;
+import lombok.experimental.UtilityClass;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+
+import static com.ricedotwho.rsm.data.Phase7.*;
+
+@UtilityClass
+public class DungeonUtils implements Accessor {
+
+    private final AABB S1Box = new AABB(
+            89, 0, 30,
+            113, 255, 122
+    );
+
+    private final AABB S2Box = new AABB(
+            19, 0, 121,
+            111, 255, 145
+    );
+
+    private final AABB S3Box = new AABB(
+            -6, 0, 50,
+            19, 255, 143
+    );
+
+    private final AABB S4Box = new AABB(
+            -2, 0, 27,
+            90, 255, 51
+    );
+    
+    public Phase7 getF7Phase() {
+        double posY = mc.player.position().y();
+        if (posY > 210) return P1;
+        if (posY > 155) return P2;
+        if (posY > 100) return P3;
+        if (posY > 45) return P4;
+        return P5;
+    }
+    
+    public Phase7 getP3Section() {
+        return getP3Section(mc.player.position());
+    }
+    
+    public Phase7 getP3Section(Vec3 pos) {
+        if (S1Box.contains(pos)) {
+            return S1;
+        }
+
+        if (S2Box.contains(pos)) {
+            return S2;
+        }
+
+        if (S3Box.contains(pos)) {
+            return S3;
+        }
+
+        if (S4Box.contains(pos)) {
+            return S4;
+        }
+
+        return UNKNOWN;
+    }
+    
+    public boolean isPhase(Phase7 phase) {
+        return getF7Phase() == phase;
+    }
+    
+    public boolean isP3Section(Phase7 section) {
+        return getP3Section() == section;
+    }
+}
