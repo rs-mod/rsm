2bd0b05eef3c32583229491b2745df24d438f09d
hypericats <hypericats@gmail.com>
2026-03-07 15:46:05 -0500

    increased max chat history

---
+++ src/main/java/com/ricedotwho/rsm/mixins/MixinChatComponent.java
@@ -0,0 +1,18 @@
+package com.ricedotwho.rsm.mixins;
+
+import net.minecraft.client.GuiMessageTag;
+import net.minecraft.client.gui.components.ChatComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = ChatComponent.class, priority = 300) // Low priority, will apply before others, so can be overriden if something tries to set it higher?
+public class MixinChatComponent {
+    @ModifyConstant(
+            method = "addRecentChat",
+            constant = @Constant(intValue = 100)
+    )
+    private int modifyMaxMessages(int original) {
+        return 10000;
+    }
+}
