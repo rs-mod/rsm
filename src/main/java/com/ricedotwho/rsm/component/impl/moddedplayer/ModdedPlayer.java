289f5e2df3c16c700c3bb1b60676bb3ad29a6f05
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-05-27 14:05:48 +1000

    optifine & custom capes, bump version

--- src/main/java/com/ricedotwho/rsm/component/impl/moddedplayer/ModdedPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.ricedotwho.rsm.component.impl.moddedplayer;
-
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-
-public record ModdedPlayer(Component name, float xScale, float yScale, float zScale, ResourceLocation cape) {
-}
