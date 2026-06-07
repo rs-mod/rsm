bf4db59fe9c333343bec6706b4e00d8340fa2acc
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-06-08 05:19:32 +1000

    yeah nvm

--- src/main/java/com/ricedotwho/rsm/component/impl/NoRotateManager.java
+++ src/main/java/com/ricedotwho/rsm/component/impl/NoRotateManager.java
@@ -6,7 +6,13 @@ import com.ricedotwho.rsm.event.api.SubscribeEvent;
 import com.ricedotwho.rsm.event.impl.world.WorldEvent;
 import com.ricedotwho.rsm.mixins.accessor.LocalPlayerAccessor;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
+import net.minecraft.network.protocol.game.ServerboundAcceptTeleportationPacket;
+import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.world.entity.PositionMoveRotation;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 
@@ -42,6 +48,33 @@ public class NoRotateManager extends ModComponent {
         ((LocalPlayerAccessor) player).setYRotLast(yRot0);
         ((LocalPlayerAccessor) player).setXRotLast(xRot0);
         reset();
+    }
+
+    public static void handleTp(ClientboundPlayerPositionPacket packet, Connection connection, CallbackInfo ci) {
+        if (!noRotatePackets.contains(packet)) return;
+        noRotatePackets.remove(packet);
+
+        LocalPlayer player = mc.player;
+        if (player == null) return;
+
+        PositionMoveRotation startPos = PositionMoveRotation.of(player);
+        PositionMoveRotation newPos = PositionMoveRotation.calculateAbsolute(startPos, packet.change(), packet.relatives());
+
+        player.setPos(newPos.position());
+        player.setDeltaMovement(newPos.deltaMovement());
+
+        PositionMoveRotation oldPlayerPos = new PositionMoveRotation(player.oldPosition(), Vec3.ZERO, player.yRotO, player.xRotO);
+        PositionMoveRotation newOldPlayerPos = PositionMoveRotation.calculateAbsolute(oldPlayerPos, packet.change(), packet.relatives());
+
+        player.setOldPosAndRot(newOldPlayerPos.position(), player.yRotO, player.xRotO); // i would prefer to just set position here, but fun is private
+
+        connection.send(new ServerboundAcceptTeleportationPacket(packet.id()));
+        connection.send(new ServerboundMovePlayerPacket.PosRot(player.getX(), player.getY(), player.getZ(), newPos.yRot(), newPos.xRot(), false, false));
+
+        ((LocalPlayerAccessor) player).setYRotLast(newPos.yRot());
+        ((LocalPlayerAccessor) player).setXRotLast(newPos.xRot());
+
+        ci.cancel();
     }
 
     private static void reset() {
