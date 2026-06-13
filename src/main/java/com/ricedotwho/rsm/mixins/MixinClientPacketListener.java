016944283c201f3bca6d13778ef03f80c99bb299
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-06-13 11:13:35 +1000

    oops

--- src/main/java/com/ricedotwho/rsm/mixins/MixinClientPacketListener.java
+++ src/main/java/com/ricedotwho/rsm/mixins/MixinClientPacketListener.java
@@ -87,7 +87,7 @@ public abstract class MixinClientPacketListener implements Accessor {
         new ChunkLoadEvent(chunk).post();
     }
 
-    @Inject(method = "handleMovePlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientPacketListener;setValuesFromPositionPacket(Lnet/minecraft/world/entity/PositionMoveRotation;Ljava/util/Set;Lnet/minecraft/world/entity/Entity;Z)Z", shift = At.Shift.BEFORE))
+    @Inject(method = "handleMovePlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientPacketListener;setValuesFromPositionPacket(Lnet/minecraft/world/entity/PositionMoveRotation;Ljava/util/Set;Lnet/minecraft/world/entity/Entity;Z)Z", shift = At.Shift.BEFORE), cancellable = true)
     private void onPreHandlePlayerMove(ClientboundPlayerPositionPacket packet, CallbackInfo ci) {
         NoRotateManager.handleTp(packet, getConnection(), ci);
     }
