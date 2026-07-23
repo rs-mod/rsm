a5aaba1ecf4e7f9991ec3e79a3690554932fffb5
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-07-23 20:03:32 +1000

    Update SwapManager.java

--- src/main/java/com/ricedotwho/rsm/component/impl/SwapManager.java
+++ src/main/java/com/ricedotwho/rsm/component/impl/SwapManager.java
@@ -56,8 +56,15 @@ public class SwapManager extends ModComponent {
     private record RequiredSwap(int slot, boolean silent) {}
 
     public static void onPreTickStart() {
-        swappedThisTick = false;
+        //swappedThisTick = false;
         requireSwap = new RequiredSwap(-1, false);
+    }
+
+    @SubscribeEvent
+    public void onTickEnd(PacketEvent.Receive event) {
+        if (event.getPacket() instanceof ServerboundClientTickEndPacket) {
+            swappedThisTick = false;
+        }
     }
 
     public static boolean onPostSendPacket(Packet<?> packet) {
